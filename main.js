12e7c1dd73d21cf30501e06bd8576b485496b926
Carruor <fotoes10@gmail.com>
2022-02-14 11:20:37 +0100

    create pokemon card function implemented

--- main.js
+++ main.js
@@ -3,7 +3,9 @@ let pokemonContainer = document.getElementById('pkmnContainer');
 function fetchPkmn(id) {
     fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
     .then(res => res.json())
-    .then(data => console.log(data));
+    .then(data => {
+        createPkmnCard(data);
+    });
 }
 
 function fetchAllPkmns(number) {
@@ -12,4 +14,31 @@ function fetchAllPkmns(number) {
     }
 }
 
-fetchAllPkmns(2);
+fetchAllPkmns(5);
+
+function createPkmnCard(pokemon) {
+    const card = document.createElement('div');
+    card.classList.add('pokemonCard');
+
+    const imgContainer = document.createElement('div');
+    imgContainer.classList.add('imgContainer');
+
+    const img = document.createElement('img');
+    img.src =pokemon.sprites.front_default;
+
+    imgContainer.appendChild(img);
+
+    const number = document.createElement('p');
+    number.textContent = `${pokemon.id}`;
+
+    const name = document.createElement('p');
+    name.classList.add('name');
+    name.textContent = pokemon.name;
+
+    card.appendChild(imgContainer);
+    card.appendChild(number);
+    card.appendChild(name); 
+
+    pokemonContainer.appendChild(card);
+}
+
